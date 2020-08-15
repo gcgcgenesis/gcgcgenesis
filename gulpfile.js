b5d39464457f1a9fcaa5f6379ede7f8c3f25f007
andrewleung93 <andrewleung93@gmail.com>
2020-08-15 10:33:39 -0400

    PROTECT THIS TIME?

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ function encrypt(password) {
 
 gulp.task('firewall:encrypt', () => {
   return gulp.src('_protected/*.*')
-    .pipe(encrypt('password'))
+    .pipe(encrypt('gracegospel'))
     .pipe(gulp.dest('_posts'));
 });
 
